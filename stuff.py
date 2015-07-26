07e21edffac175ae9f718aa6f3e3c95438947f9a
Pekka Pöyry <pekka.poyry@fastmonkeys.com>
2015-07-26 13:59:39 +0300

    Better handling of error cases

--- stuff.py
+++ stuff.py
@@ -1,4 +1,5 @@
 import sys
+from math import ceil
 
 import numpy as np
 import cv2
@@ -36,7 +37,7 @@ def extract_black_color_as_new_image(img):
 
 def extract_coffee_color_as_new_image(img):
     lower = np.array([0, 0, 0], dtype="uint8")
-    upper = np.array([35, 35, 35], dtype="uint8")
+    upper = np.array([45, 45, 45], dtype="uint8")
 
     mask = cv2.inRange(img, lower, upper)
     mask_inv = cv2.bitwise_not(mask)
@@ -72,59 +73,142 @@ def get_sub_image(img, top_left, bottom_right):
     return img[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0]]
 
 
-def get_center_pole_location(img):
+def get_center_pole_location(img, pot_tl, pot_br):
     pole_image = cv2.imread('sample_images/sample2.png', cv2.IMREAD_COLOR)
-    w, h, d = pole_image.shape
-    STRIP_SIDES = 0
-    # img = cv2.resize(img, (50, 1), interpolation=cv2.INTER_AREA)
+    h, w, d = pole_image.shape
+    img_pot = extract_black_color_as_new_image(
+        get_sub_image(img, pot_tl, pot_br)
+    )
+    pot_w = pot_br[0] - pot_tl[0]
+    pos_x = max(pot_tl[0] - 2*pot_w, 0)
 
-    # img = img[:, STRIP_SIDES:-STRIP_SIDES-1]
-    # cv2.imshow('image', img)
-    # cv2.waitKey(0)
-    # cv2.imshow('image', pole_image)
-    # cv2.waitKey(0)
-    # res = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img = extract_black_color_as_new_image(img)
-    res = cv2.matchTemplate(img, pole_image, cv2.TM_CCOEFF)
-    # cv2.imshow('image', res)
-    # # cv2.imshow('image', cv2.resize(res, (0,0), fy=100, fx=1, interpolation=cv2.INTER_NEAREST))
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
+    img[pot_tl[1]:pot_br[1], pos_x:pos_x + pot_w] = img_pot
+    res = cv2.matchTemplate(img_pot, pole_image, cv2.TM_CCOEFF)
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
     top_left = max_loc
+    real_top_left = (
+        pot_tl[0] + top_left[0],
+        pot_tl[1] + top_left[1]
+    )
+    CENTER_POLE_MATCH_THRESHOLD = 10000000
+    CENTER_POLE_MATCH2_THRESHOLD = -100000
+    pole_image2 = cv2.imread('sample_images/sample3.png', cv2.IMREAD_COLOR)
+    h2, w2, d2 = pole_image2.shape
+    img_src = get_sub_image(
+        img_pot,
+        (max_loc[0] + w/2 - ceil(w2/2), max_loc[1]),
+        (max_loc[0] + w/2 + ceil(w2/2), max_loc[1] + h2)
+    )
 
-    # print cv2.imwrite('temp/debug_source.png', img)
-    # print cv2.imwrite('temp/debug_template.png', pole_image)
-    # print cv2.imwrite('temp/debug_output.png', res)
+    cv2.rectangle(
+        img,
+        (real_top_left[0] + w/2 - w2/2, real_top_left[1]),
+        (real_top_left[0] + w/2 + w2/2, real_top_left[1] + h2),
+        (0, 0, 255),
+        2
+    )
+    # import pudb;pu.db
+    res = cv2.matchTemplate(img_src, pole_image2, cv2.TM_CCOEFF)
+    min_val, max_val2, min_loc, max_loc = cv2.minMaxLoc(res)
 
+    # cv2.imshow('image', cv2.resize(pole_image2, (0,0), fy=10, fx=10, interpolation=cv2.INTER_NEAREST))
+    # cv2.waitKey(0)
+    # cv2.imshow('image', cv2.resize(img_pot, (0,0), fy=10, fx=10, interpolation=cv2.INTER_NEAREST))
+    # cv2.waitKey(0)
+    # cv2.imshow('image', cv2.resize(img_src, (0,0), fy=10, fx=10, interpolation=cv2.INTER_NEAREST))
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
 
     # import pudb;pu.db
 
-    top_left = (top_left[0] + STRIP_SIDES + w/2, top_left[1])
+    # print "initial: %s second: %s" % (max_val, max_val2)
 
-    return top_left
+    return (
+        real_top_left[0] + w/2,
+        real_top_left[1]
+    ), (
+        (max_val > CENTER_POLE_MATCH_THRESHOLD) and
+        (max_val2 > CENTER_POLE_MATCH2_THRESHOLD)
+    )
 
 
 def get_coffee_level(img, position, name):
     def measure_coffee_level(line):
         # import pudb;pu.db
-        return sum(1 for l in line if not all(l[0]))
+        return sum(
+            1 for l in line
+            if not all(l[0])), not all(not x.all() for x in line[:3])
     cv2.imwrite('temp2/%s_debug_source.png' % name, img)
-    img = extract_coffee_color_as_new_image(img)
-    cv2.imwrite('temp2/%s_debug_output.png' % name, img)
+    img_bw = extract_coffee_color_as_new_image(img)
+    cv2.imwrite('temp2/%s_debug_output.png' % name, img_bw)
 
-    X_OFFSETS = [4, -4, 8, -8]
+    X_OFFSETS = [5, -5, 15, -15]
     Y_OFFSET = 6
     Y_MAX = 45
 
     lines = [
-        img[
-            position[1] + Y_OFFSET:position[1] + Y_MAX,
-            position[0] - x_offset:position[0] - x_offset+1
-        ] for x_offset in X_OFFSETS
+        (
+            (position[1] + Y_OFFSET, position[1] + Y_MAX),
+            (position[0] - x_offset, position[0] - x_offset)
+        ) for x_offset in X_OFFSETS
     ]
-    cv2.imwrite('temp2/%s_debug_output2.png' % name, lines[0])
-    return [measure_coffee_level(line) for line in lines]
+    # cv2.rectangle(img, center_tl, center_br, (0, 0, 255), 2)
+    line_images = [
+        img_bw[p[0][0]:p[0][1], p[1][0]:p[1][1]+1] for p in lines
+    ]
+    results = [measure_coffee_level(line) for line in line_images]
+    for result, line in zip(results, lines):
+        color = (0, 255, 0)
+        if not result[1]:
+            color = (0, 0, 255)
+        cv2.rectangle(
+            img,
+            (line[1][0], line[0][0]),
+            (line[1][1], line[0][1]),
+            color,
+            1
+        )
+        if result[1]:
+            cv2.rectangle(
+                img,
+                (line[1][0]-1, line[0][1] - result[0]),
+                (line[1][1]+1, line[0][1] - result[0]),
+                (0, 255, 255),
+                1
+            )
+    results = sorted(r[0] for r in results if r[1])
+
+    if len(results) > 1:
+        result = results[1]
+    else:
+        print "Handle detection is too sensitive"
+        if results:
+            result = results[0]
+        else:
+            result = 0
+
+    # import pudb;pu.db
+    IMG_X_OFFSET = -40
+    IMG_W = 10
+    y_top = position[1] + Y_OFFSET
+    y_middle = position[1] + Y_MAX - result
+    y_bottom = position[1] + Y_MAX
+    cv2.rectangle(
+        img,
+        (position[0] + IMG_X_OFFSET, y_top),
+        (position[0] + IMG_X_OFFSET + IMG_W, y_middle),
+        (255, 255, 0),
+        -1
+    )
+    cv2.rectangle(
+        img,
+        (position[0] + IMG_X_OFFSET, y_middle),
+        (position[0] + IMG_X_OFFSET + IMG_W, y_bottom),
+        (0, 0, 0),
+        -1
+    )
+
+    return result, result/7
 
 
 for img_file in sys.argv[1:]:
@@ -141,17 +225,26 @@ for img_file in sys.argv[1:]:
         bottom_right
     )
 
-    img_pot = get_sub_image(img, pot_tl, pot_br)
-
     # img_pot = cv2.Canny(img_pot,100,200)
+
+
+    pos, match = get_center_pole_location(img, pot_tl, pot_br)
+    # print "Pot pos %r %r" % pos
+    center_tl = pos[0], pos[1]
+    center_br = pos[0], pos[1] + 50
+
+    if match:
+        result = get_coffee_level(img, pos, img_file.split('/')[1].split('.')[0])
+        print "%s: %s" % (result, img_file)
+        # cv2.rectangle(img, center_tl, center_br, (255, 255, 255), 2)
+    else:
+        # cv2.rectangle(img, center_tl, center_br, (0, 0, 255), 2)
+        print "No pot: %s" % img_file
+
+    # print "GFROM %s %s to %s %s" % (center_tl[0],center_tl[1], center_br[0],center_br[1])
 
     cv2.rectangle(img, top_left, bottom_right, (255, 0, 0), 2)
     cv2.rectangle(img, pot_tl, pot_br, (200, 200, 200), 2)
-
-    pos = get_center_pole_location(img_pot)
-    # print "Pot pos %r %r" % pos
-
-    print "%s: %s" % (get_coffee_level(img_pot, pos, img_file.split('/')[1].split('.')[0]), img_file)
 
     # cv2.imshow('image', img_pot)
     # cv2.waitKey(0)
@@ -160,12 +253,8 @@ for img_file in sys.argv[1:]:
     # cv2.waitKey(0)
     # cv2.destroyAllWindows()
 
-
-    center_tl = pot_tl[0] + pos[0], pot_tl[1] + pos[1]
-    center_br = pot_tl[0] + pos[0], pot_tl[1] + pos[1] + 50
-
-    cv2.rectangle(img, center_tl, center_br, (0, 0, 255), 2)
-
+    # img = get_sub_image(img, top_left, bottom_right)
+    # img = cv2.resize(img, (0,0), fy=10, fx=10, interpolation=cv2.INTER_NEAREST)
 
     our_file = 'temp/' + img_file.split('/')[1].split('.')[0] + '.png'
     # print "Writing", our_file
