d2497f5f8f146796b3a63b9eeeb19ab268b7877b
Pekka Pöyry <pekka.poyry@fastmonkeys.com>
2015-08-28 21:00:45 +0300

    Cleaning code

--- stuff.py
+++ stuff.py
@@ -61,13 +61,9 @@ def get_sub_image(img, top_left, bottom_right):
 
 
 def get_coffee_pot_aabb(img_pot, pot_tl, pot_br, sample_img):
-    pot_w = pot_br[0] - pot_tl[0]
-    pos_x = max(pot_tl[0] - 2*pot_w, 0)
-
-    # img[pot_tl[1]:pot_br[1], pos_x:pos_x + pot_w] = img_pot
     img_pot_det = get_sub_image(img_pot, (0, 0), (pot_br[0] - pot_tl[0], 14))
     res = cv2.matchTemplate(img_pot_det, sample_img, cv2.TM_CCOEFF)
-    # cv2.imwrite('temp2/img_pot_res.png', res)
+
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
     top_left = max_loc
     real_top_left = (
@@ -115,47 +111,7 @@ def get_coffee_pot_location(img, pot_tl, pot_br, img_file):
         our_file = 'temp/fail_pot_' + img_file.split('/')[1].split('.')[0] + '.png'
         cv2.imwrite(our_file, img_pot_org)
 
-    # pole_image2 = cv2.imread('sample_images/sample3.png', cv2.IMREAD_COLOR)
-    # h2, w2, d2 = pole_image2.shape
-
-    # # img_pot2 = cv2.Canny(img_pot_org, 200, 500)
-    # img_pot2 = img_pot_org
-
-    # # cv2.imwrite('temp2/img_pot2.png', img_pot2)
-    # img_src = get_sub_image(
-    #     img_pot2,
-    #     (max_loc[0] + w/2 - ceil(w2/2.0), max_loc[1]),
-    #     (max_loc[0] + w/2 + ceil(w2/2.0), max_loc[1] + h2)
-    # )
-
-    # # cv2.rectangle(
-    # #     img,
-    # #     (real_top_left[0] + w/2 - w2/2, real_top_left[1]),
-    # #     (real_top_left[0] + w/2 + w2/2, real_top_left[1] + h2),
-    # #     (0, 0, 255),
-    # #     2
-    # # )
-    # import pudb;pu.db
-    # res = cv2.matchTemplate(img_src, pole_image2, cv2.TM_CCOEFF)
-    # min_val, max_val2, min_loc, max_loc = cv2.minMaxLoc(res)
-
-    # cv2.imshow('image', cv2.resize(pole_image2, (0,0), fy=10, fx=10, interpolation=cv2.INTER_NEAREST))
-    # cv2.waitKey(0)
-    # cv2.imshow('image', cv2.resize(img_pot, (0,0), fy=10, fx=10, interpolation=cv2.INTER_NEAREST))
-    # cv2.waitKey(0)
-    # cv2.imshow('image', cv2.resize(img_src, (0,0), fy=10, fx=10, interpolation=cv2.INTER_NEAREST))
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-
-    # import pudb;pu.db
-
-    # print "initial: %s second: %s" % (max_val, max_val2)
-
-    return (
-        real_top_left,
-        ok #and
-        # (max_val2 > CENTER_POLE_MATCH2_THRESHOLD)
-    )
+    return real_top_left, ok
 
 
 def get_coffee_level(img, position, name):
@@ -174,23 +130,20 @@ def get_coffee_level(img, position, name):
         (position[0] - X_RANGE, position[1]),
         (position[0] + X_RANGE, position[1] + Y_DELTA)
     )
-    # print img_bw.shape
 
     our_file = 'temp2/coffee_avg_src.png'
     cv2.imwrite(our_file, img_bw)
 
-    # img_asd = cv2.resize(img_bw, (1,Y_MAX), interpolation=cv2.INTER_AREA)
-    # print img_asd.shape
-    img_asd = cv2.reduce(img_bw, 1, cv2.cv.CV_REDUCE_AVG)
-    # print img_asd.shape
-    img_asd2 = cv2.resize(
-        img_asd,
+    # img_coffee_level = cv2.resize(img_bw, (1,Y_MAX), interpolation=cv2.INTER_AREA)
+    img_coffee_level = cv2.reduce(img_bw, 1, cv2.cv.CV_REDUCE_AVG)
+    img_coffee_level_big = cv2.resize(
+        img_coffee_level,
         (IMG_W, Y_DELTA),
         interpolation=cv2.INTER_AREA
     )
 
     COFFEE_SKIP_HEIGHT = 3
-    levels = [img_asd[i][0][0] for i in range(img_bw.shape[0])]
+    levels = [img_coffee_level[i][0][0] for i in range(img_bw.shape[0])]
     threshold = sum(levels[0:COFFEE_SKIP_HEIGHT]) / COFFEE_SKIP_HEIGHT * 0.65
 
     for i, v in enumerate(levels):
@@ -230,7 +183,7 @@ def get_coffee_level(img, position, name):
     img[
         y_top:y_top+Y_DELTA,
         position[0] + IMG_X_OFFSET - IMG_W:position[0] + IMG_X_OFFSET - IMG_W + 10
-    ] = img_asd2
+    ] = img_coffee_level_big
 
     return int(result * 100 / Y_MAX)
 
@@ -257,44 +210,30 @@ def process_image(img, img_file):
         bottom_right
     )
 
-    # img_pot = cv2.Canny(img_pot,100,200)
-
-
     pos, match = get_coffee_pot_location(img, pot_tl, pot_br, img_file)
-    # print "Pot pos %r %r" % pos
     center_tl = pos[0], pos[1]
     center_br = pos[0], pos[1] + 30
 
     if match:
-        result = get_coffee_level(img, pos, img_file.split('/')[1].split('.')[0])
+        result = get_coffee_level(
+            img,
+            pos,
+            img_file.split('/')[1].split('.')[0]
+        )
         print "%s: %s" % (result, img_file)
         cv2.rectangle(img, center_tl, center_br, (255, 255, 255), 2)
     else:
         cv2.rectangle(img, center_tl, center_br, (0, 0, 255), 2)
         print "No pot: %s" % img_file
 
-    # print "GFROM %s %s to %s %s" % (center_tl[0],center_tl[1], center_br[0],center_br[1])
-
     cv2.rectangle(img, top_left, bottom_right, (255, 0, 0), 2)
     cv2.rectangle(img, pot_tl, pot_br, (200, 200, 200), 2)
 
-    # cv2.imshow('image', img_pot)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-    # cv2.imshow('image', cv2.resize(img3, (0,0), fy=100, fx=1, interpolation=cv2.INTER_NEAREST))
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-
-    # img = get_sub_image(img, top_left, bottom_right)
-    # img = cv2.resize(img, (0,0), fy=10, fx=10, interpolation=cv2.INTER_NEAREST)
     return img
 
 
 for img_file in sys.argv[1:]:
-    # Load an color image in grayscale
-    # print "Opening", img_file
     img = cv2.imread(img_file, cv2.IMREAD_COLOR)
     img = process_image(img, img_file)
     our_file = 'temp/' + img_file.split('/')[1].split('.')[0] + '.png'
-    # print "Writing", our_file
     cv2.imwrite(our_file, img)
