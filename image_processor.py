3f19ff2c3a599bb93e80554b824848ddae9ae7e6
Pekka Pöyry <pekka.poyry@fastmonkeys.com>
2016-09-02 20:38:32 +0300

    New office, new coffee maker, rebuild everything

--- image_processor.py
+++ image_processor.py
@@ -1,13 +1,16 @@
 import sys
 
 import numpy as np
+
+sys.path.append('/usr/local/lib/python2.7/site-packages')
 import cv2
 
+debugging = False
+
+COFFEE_MAKER_MASK_FILE = 'sample_images/coffee_maker_sample_mask.png'
 
 COFFEE_MAKES_COLOR_RANGES = [
-    ((0, 80), (0, 80), (50, 255)),
-    ((0, 100), (0, 100), (50, 255)),
-    ((0, 20), (0, 20), (20, 100)),
+    ((0, 80), (0, 80), (0, 80)),
 ]
 
 COFFEE_POT_TOP_COLOR_RANGES = [
@@ -36,8 +39,7 @@ def extract_color_as_new_image(img, limit):
     return img3
 
 
-def get_coffee_maker_aabb(img):
-    template = cv2.imread('sample_images/sample.png', cv2.IMREAD_COLOR)
+def match_template(img, template):
     w, h = template.shape[1], template.shape[0]
     res = cv2.matchTemplate(img, template, cv2.TM_CCOEFF)
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
@@ -46,13 +48,9 @@ def get_coffee_maker_aabb(img):
     return top_left, bottom_right, max_val
 
 
-def get_coffee_pot_location_offset(img, top_left, bottom_right):
-    offset = (30, 12)
-    offset = (offset[0] + top_left[0], offset[1] + top_left[1])
-    size = (42, 36)
-    top_left = (offset[0], offset[1])
-    bottom_right = (offset[0]+size[0], offset[1]+size[1])
-    return top_left, bottom_right
+def get_coffee_maker_aabb(img):
+    template = cv2.imread(COFFEE_MAKER_MASK_FILE, cv2.IMREAD_COLOR)
+    return match_template(img, template)
 
 
 def get_sub_image(img, top_left, bottom_right):
@@ -77,164 +75,153 @@ def get_coffee_pot_aabb(img_pot, pot_tl, pot_br, sample_img):
     ), max_val
 
 
-def get_coffee_pot_location(img, pot_tl, pot_br, img_file):
-    pole_image = cv2.imread('sample_images/sample2.png', cv2.IMREAD_COLOR)
-    img_pot_org = get_sub_image(img, pot_tl, pot_br)
-    cv2.imwrite('debug_output/coffee_pot_org.png', img_pot_org)
+def get_coffee_level(img):
+    Y_MAX = img.shape[0]
 
-    places = []
+    img_coffee_level = cv2.reduce(img, 1, cv2.cv.CV_REDUCE_AVG)
+    debug(img_coffee_level, 'coffee_pot_reduced')
 
-    for i, limits in enumerate(COFFEE_POT_TOP_COLOR_RANGES):
-        img_pot = extract_color_as_new_image(img_pot_org, limits)
-        cv2.imwrite('debug_output/coffee_pot_bw_%d.png' % i, img_pot)
-        top_left, max_val = get_coffee_pot_aabb(img_pot, pot_tl, pot_br, pole_image)
-        img_normal = img.copy()
-        bottom_right = (top_left[0], top_left[0] + 30)
-        cv2.rectangle(img_normal, top_left, bottom_right, (255, 0, 0), 2)
-        cv2.imwrite('debug_output/coffee_maker_pos_%d.png' % i, img_normal)
-        places.append((max_val, top_left))
-
-    max_val, real_top_left = sorted(places, key=lambda x: x[0])[-1]
-    max_val = int(max_val / 10000)
-
-    CENTER_POLE_MATCH_THRESHOLD = 550
-    print("Pot match: %d/%d" % (max_val, CENTER_POLE_MATCH_THRESHOLD))
-
-    ok = max_val > CENTER_POLE_MATCH_THRESHOLD
-    if ok:
-        our_file = 'debug_output/ok_pot_' + img_file.split('/')[1].split('.')[0] + '.png'
-        cv2.imwrite(our_file, img_pot_org)
-    else:
-        our_file = 'debug_output/fail_pot_' + img_file.split('/')[1].split('.')[0] + '_bw.png'
-        cv2.imwrite(our_file, img_pot)
-        our_file = 'debug_output/fail_pot_' + img_file.split('/')[1].split('.')[0] + '.png'
-        cv2.imwrite(our_file, img_pot_org)
-
-    return real_top_left, ok
-
-
-def get_coffee_level(img, position, name):
-    IMG_X_OFFSET = -30
-    IMG_W = 10
-
-    X_RANGE = 10
-    Y_OFFSET = 4
-    Y_MAX = 29
-    Y_DELTA = Y_MAX - Y_OFFSET
-
-    position = (position[0], position[1] + Y_OFFSET)
-
-    img_bw = get_sub_image(
-        img,
-        (position[0] - X_RANGE, position[1]),
-        (position[0] + X_RANGE, position[1] + Y_DELTA)
-    )
-
-    our_file = 'debug_output/coffee_avg_src.png'
-    cv2.imwrite(our_file, img_bw)
-
-    # img_coffee_level = cv2.resize(img_bw, (1,Y_MAX), interpolation=cv2.INTER_AREA)
-    img_coffee_level = cv2.reduce(img_bw, 1, cv2.cv.CV_REDUCE_AVG)
-    img_coffee_level_big = cv2.resize(
-        img_coffee_level,
-        (IMG_W, Y_DELTA),
-        interpolation=cv2.INTER_AREA
-    )
-
-    COFFEE_SKIP_HEIGHT = 3
-    levels = [img_coffee_level[i][0][0] for i in range(img_bw.shape[0])]
-    threshold = sum(levels[0:COFFEE_SKIP_HEIGHT]) / COFFEE_SKIP_HEIGHT * 0.65
+    levels = [
+        img_coffee_level[i][0][0] for i in range(img_coffee_level.shape[0])
+    ]
+    COFFEE_SKIP_HEIGHT = 1 # TODO: How much we have certain black to measure blackness threshold ***********
+    threshold = sum(levels[0:COFFEE_SKIP_HEIGHT]) / COFFEE_SKIP_HEIGHT * 0.80
 
     for i, v in enumerate(levels):
-        if i < 3:
-            continue
         # print "i:%d %r" % (i, v)
         if v < threshold:
             break
-    result = Y_DELTA - i
+    result = Y_MAX - i
     print("Coffee: %d/%d" % (result, Y_MAX - COFFEE_SKIP_HEIGHT))
 
-    y_top = position[1]
-    y_middle = position[1] + Y_DELTA - result
-    y_bottom = position[1] + Y_DELTA
-    cv2.rectangle(
-        img,
-        (position[0] + IMG_X_OFFSET-1, y_top-1),
-        (position[0] + IMG_X_OFFSET + IMG_W+1, y_bottom+1),
-        (255, 255, 255),
-        -1
-    )
-    cv2.rectangle(
-        img,
-        (position[0] + IMG_X_OFFSET, y_top),
-        (position[0] + IMG_X_OFFSET + IMG_W, y_middle),
-        (255, 255, 0),
-        -1
-    )
-    cv2.rectangle(
-        img,
-        (position[0] + IMG_X_OFFSET, y_middle),
-        (position[0] + IMG_X_OFFSET + IMG_W, y_bottom),
-        (0, 0, 0),
-        -1
-    )
-
-    img[
-        y_top:y_top+Y_DELTA,
-        position[0] + IMG_X_OFFSET - IMG_W:position[0] + IMG_X_OFFSET - IMG_W + 10
-    ] = img_coffee_level_big
-
-    return int(result * 100 / Y_MAX)
+    return int(result * 100 / Y_MAX), img_coffee_level
 
 
 def process_image(img, img_file):
-    img = get_sub_image(img, (520, 200), (720, 350))
-
     places = []
 
     for i, limits in enumerate(COFFEE_MAKES_COLOR_RANGES):
         img3 = extract_color_as_new_image(img, limits)
-        cv2.imwrite('debug_output/coffee_maker_bw_%d.png' % i, img3)
+        debug(img3, 'coffee_maker_bw_%d' % i)
         top_left, bottom_right, max_val = get_coffee_maker_aabb(img3)
         img_normal = img.copy()
         cv2.rectangle(img_normal, top_left, bottom_right, (255, 0, 0), 2)
-        cv2.imwrite('debug_output/coffee_maker_pos_%d.png' % i, img_normal)
+        debug(img_normal, 'coffee_maker_pos_%d' % i)
         places.append((max_val, top_left, bottom_right))
 
-    _, top_left, bottom_right = sorted(places, key=lambda x: x[0])[-1]
+    score, top_left, bottom_right = sorted(places, key=lambda x: x[0])[-1]
 
-    pot_tl, pot_br = get_coffee_pot_location_offset(
-        img,
-        top_left,
-        bottom_right
+    img = get_sub_image(img, top_left, bottom_right)
+
+    print("Coffee maker mask score: %s" % score)
+    debug(img, 'coffee_maker_pos')
+
+    pot_tl, pot_br = (70, 90), (120, 170)
+    pot_header_tl, pot_header_br = (0, 0), (50, 20)
+
+    img_pot_with_top = get_sub_image(img, pot_tl, pot_br)
+
+    h, w, d = img_pot_with_top.shape
+
+    img_pot_confirmer = get_sub_image(
+        img_pot_with_top.copy(),
+        pot_header_tl,
+        pot_header_br
+    )
+    img_pot = get_sub_image(
+        img_pot_with_top.copy(),
+        (0, pot_header_br[1]),
+        (w, h)
     )
 
-    pos, match = get_coffee_pot_location(img, pot_tl, pot_br, img_file)
-    center_tl = pos[0], pos[1]
-    center_br = pos[0], pos[1] + 30
+    cv2.rectangle(img_pot_with_top, pot_header_tl, pot_header_br, (255, 0, 0), 2)
+
+    debug(img_pot_with_top, 'coffee_pot_pos')
+
+    # Why this isn't working??? *****************************************************************************
+    black_template = np.zeros(img_pot_confirmer.shape, np.uint8)
+
+    _, _, score = match_template(img_pot_confirmer, black_template)
+
+    print("Pot confirmer mask score: %s" % score)
 
     result = None
-    if match:
-        result = get_coffee_level(
-            img,
-            pos,
-            img_file.split('/')[1].split('.')[0]
-        )
+    result, coffee_level_image = get_coffee_level(img_pot)
+    if result is not None:
         print("%s: %s" % (result, img_file))
-        cv2.rectangle(img, center_tl, center_br, (255, 255, 255), 2)
     else:
-        cv2.rectangle(img, center_tl, center_br, (0, 0, 255), 2)
         print("No pot: %s" % img_file)
 
-    cv2.rectangle(img, top_left, bottom_right, (255, 0, 0), 2)
     cv2.rectangle(img, pot_tl, pot_br, (200, 200, 200), 2)
+
+    h, w, _ = img.shape
+    COFFEE_IMG_WIDTH = 10
+    coffee_img_height = coffee_level_image.shape[0]
+
+    coffee_level_image = cv2.resize(
+        coffee_level_image,
+        (COFFEE_IMG_WIDTH, coffee_img_height),
+        interpolation=cv2.INTER_AREA
+    )
+
+    img[
+        pot_br[1] - coffee_img_height:pot_br[1],
+        w-COFFEE_IMG_WIDTH:w
+    ] = coffee_level_image
+
+    cv2.rectangle(
+        img,
+        (w-2*COFFEE_IMG_WIDTH, pot_br[1] - result*coffee_img_height/100),
+        (w-COFFEE_IMG_WIDTH, pot_br[1]),
+        (0, 0, 0),
+        -1
+    )
+
+    cv2.rectangle(
+        img,
+        (w-2*COFFEE_IMG_WIDTH, pot_br[1] - coffee_img_height),
+        (w-COFFEE_IMG_WIDTH, pot_br[1] - result*coffee_img_height/100),
+        (0, 0, 255),
+        -1
+    )
 
     return img, result
 
 
+def debug(img, step):
+    if debugging:
+        filename = 'debug_output/%s.png' % step
+        cv2.imwrite(filename, img)
+        print('Wrote %s' % filename)
+
+
+def rotate_image(img, degrees):
+    rows, cols, _ = img.shape
+    return cv2.warpAffine(
+        img,
+        cv2.getRotationMatrix2D((cols/2, rows/2), degrees, 1),
+        (cols, rows)
+    )
+
+
+def preprocess_image(img):
+    img = rotate_image(img, -90)
+    img = get_sub_image(img, (650, 130), (960, 350))
+    return img
+
+
 if __name__ == "__main__":
-    for img_file in sys.argv[1:]:
+    start = 1
+    args = [a for a in sys.argv[1:]]
+    if '-d' in args:
+        debugging = True
+        args = [a for a in args if a != '-d']
+
+    for img_file in args:
         img = cv2.imread(img_file, cv2.IMREAD_COLOR)
+        img = preprocess_image(img)
+        debug(img, 'after_preprocess')
         img, value = process_image(img, img_file)
+
         our_file = 'debug_output/' + img_file.split('/')[1].split('.')[0] + '.png'
         cv2.imwrite(our_file, img)
